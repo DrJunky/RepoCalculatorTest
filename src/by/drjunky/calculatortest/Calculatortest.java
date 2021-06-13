63f3e5e030f05a4d7618489beedca78ebc636f99
DrJunky <darklostmeetthejunky@gmail.com>
2021-06-13 21:08:22 +0300

    adding files

---
+++ src/by/drjunky/calculatortest/Calculatortest.java
@@ -0,0 +1,7 @@
+package by.drjunky.calculatortest;
+
+public class Calculatortest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/drjunky/hello_test/Hello_dear_user.java
@@ -0,0 +1,7 @@
+package by.drjunky.hello_test;
+
+public class Hello_dear_user {
+    public static void main(String[] args) {
+        System.out.println("Hello, dear user!");
+    }
+}
